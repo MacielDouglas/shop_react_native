3437f48ebc56eab860ae2125785352215e9c1c89
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-01-04 09:17:57 -0300

    First commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: false, title: "Shopping Native" }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: true, title: "Categorias" }}
+      />
+      <Stack.Screen
+        name="product"
+        options={{ headerShown: true, title: "Produtos" }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{ presentation: "modal", title: "Carrinho Compras" }}
+      />
+      <Stack.Screen name="auth" options={{ headerShown: true }} />
+    </Stack>
+  );
+}
