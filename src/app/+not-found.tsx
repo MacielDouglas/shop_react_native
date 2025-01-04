b36dca066ff26d3a649174af87bea096a08025c3
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-01-04 16:07:35 -0300

    Adiciona tela de erro 404 com link para a página inicial

---
+++ src/app/+not-found.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Ops! Esta tela não existe" }} />
+      <View style={styles.container}>
+        <Link href="/">Ir para a tela inicial</Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
