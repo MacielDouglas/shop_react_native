1d022949e00b6bcf38f57daeb1c4542336165065
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-01-07 15:58:55 -0300

    Adiciona suporte a notificações de toast e implementa tela de detalhes do produto com gerenciamento de quantidade no carrinho

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,28 @@
 import { Stack } from "expo-router";
+import { ToastProvider } from "react-native-toast-notifications";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ headerShown: false, title: "Shopping Native" }}
-      />
-      <Stack.Screen
-        name="categories"
-        options={{ headerShown: false, title: "Categorias" }}
-      />
-      <Stack.Screen
-        name="product"
-        options={{ headerShown: true, title: "Produtos" }}
-      />
-      <Stack.Screen
-        name="cart"
-        options={{ presentation: "modal", title: "Carrinho Compras" }}
-      />
-      <Stack.Screen name="auth" options={{ headerShown: true }} />
-    </Stack>
+    <ToastProvider>
+      <Stack>
+        <Stack.Screen
+          name="(shop)"
+          options={{ headerShown: false, title: "Shopping Native" }}
+        />
+        <Stack.Screen
+          name="categories"
+          options={{ headerShown: false, title: "Categorias" }}
+        />
+        <Stack.Screen
+          name="product"
+          options={{ headerShown: false, title: "Produtos" }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{ presentation: "modal", title: "Carrinho Compras" }}
+        />
+        <Stack.Screen name="auth" options={{ headerShown: true }} />
+      </Stack>
+    </ToastProvider>
   );
 }
